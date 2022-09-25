f9dc1a7e8e903166418fa1c16ea6962267778de8
Mona Lisa <lisa.nadeem@outlook.de>
2022-09-26 01:53:42 +0200

    added modules from the Material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,35 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+// to use the Material modules (the Angular module that exports everything necessary to use the Material elements) in your component
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { FormsModule } from "@angular/forms";
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+
+// any features of Material Design that should be used in application will need to be included in app.module.ts file
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  declarations: [AppComponent, UserRegistrationFormComponent, UserLoginFormComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
