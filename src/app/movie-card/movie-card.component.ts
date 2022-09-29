eaadfe33e4ed3b3ac90b096fc82a1f728aad544f
Mona Lisa <lisa.nadeem@outlook.de>
2022-09-29 14:11:29 +0200

    defined getMovies function in movie-card.component TS file

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -8,3 +8,34 @@ import { SynopsisComponent } from "../synopsis/synopsis.component";
 import { MatDialog } from "@angular/material/dialog";
 import { MatSnackBar } from "@angular/material/snack-bar";
 
+@Component({
+  selector: "app-movie-card",
+  templateUrl: "./movie-card.component.html",
+  styleUrls: ["./movie-card.component.scss"],
+})
+export class MovieCardComponent implements OnInit {
+  // variable movies declared as an array
+  // this is where the movies returned from the API call will be kept
+  movies: any[] = [];
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar
+  ) {}
+
+  // getMovies() is called in the ngOnInit() lifecycle hook
+  // ngOnInit() is called when Angular is done creating the component (in React ComponentDidMount)
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  // gets movies from api call and sets the movies state to return JSON file
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
+
