fd5b6bccdb42f8b57136d4be9c41e71a571d9d95
Mona Lisa <lisa.nadeem@outlook.de>
2022-09-29 13:57:21 +0200

    removed UserRegistration and UserLogin code for separate component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-// app's root component
 import { Component } from "@angular/core";
-import { UserRegistrationFormComponent } from "./user-registration-form/user-registration-form.component";
-import { UserLoginFormComponent } from "./user-login-form/user-login-form.component";
-import { MatDialog } from "@angular/material/dialog";
 
 @Component({
   selector: "app-root",
@@ -10,23 +6,5 @@ import { MatDialog } from "@angular/material/dialog";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "bolly-flix-Angular-client";
-
-  // passed Angular Material dialog in the constructor as an argument so that it's available for use in this component
-  constructor(public dialog: MatDialog) {}
-  // this is the function that will open the dialog when the signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // assign the dialog a width
-      width: "280px",
-    });
-  }
-
-  // open user login dialog when login button is clicked
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      // assign dialog width
-      width: "280px",
-    });
-  }
+  title = "bolly-flix-angular-client";
 }
