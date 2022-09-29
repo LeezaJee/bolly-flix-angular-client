b75e6dc66818121d48b74e16b902ee6c5b5abff3
Mona Lisa <lisa.nadeem@outlook.de>
2022-09-29 14:08:12 +0200

    added registerUser function to TS file of user-registration-form.component

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -9,7 +9,7 @@ import { FetchApiDataService } from "../fetch-api-data.service";
 // This import is used to display notifications back to the user
 import { MatSnackBar } from "@angular/material/snack-bar";
 
-// @Component decorator tells Angular that the class below is a component
+// @Component decorator tells Angular that class below is a component
 // contains instructions for wiring up the class with its stylesheet and template file
 @Component({
   // selector property defines the custom HTML element, into which this component will render
@@ -27,10 +27,10 @@ export class UserRegistrationFormComponent implements OnInit {
     public snackBar: MatSnackBar
   ) {}
 
-  // ngOnInit method is called once the component has received all its inputs (all its data-bound properties) from the calling component (user)
+  // ngOnInit method is called once the component has received all inputs (all its data-bound properties) from the calling component (user)
   ngOnInit(): void {}
 
-  // This is the function responsible for sending the form inputs to the backend
+  // this is the function responsible for sending the form inputs to the backend
   registerUser(): void {
     // userDate object from above will then be passed into API call
     this.fetchApiData.userRegistration(this.userData).subscribe(
@@ -38,13 +38,13 @@ export class UserRegistrationFormComponent implements OnInit {
         // Logic for a successful user registration goes here! (To be implemented)
         this.dialogRef.close(); // This will close the modal on success!
         console.log(result);
-        this.snackBar.open(result, "OK", {
+        this.snackBar.open("Registration successful", "OK", {
           duration: 2000,
         });
       },
       (result) => {
         console.log(result);
-        this.snackBar.open(result, "OK", {
+        this.snackBar.open("Registration failed", "OK", {
           duration: 2000,
         });
       }
