7d5aaec4aa6948f3263519dfe725758947553f4b
Mona Lisa <lisa.nadeem@outlook.de>
2022-09-29 14:10:53 +0200

    added imported statements to movie-card.component TS file

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+import { FetchApiDataService } from "../fetch-api-data.service";
+
+import { GenreComponent } from "../genre/genre.component";
+import { DirectorComponent } from "../director/director.component";
+import { SynopsisComponent } from "../synopsis/synopsis.component";
+
+import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
