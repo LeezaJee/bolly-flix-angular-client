4420cffa0c60d060bde3c761832e4655c06f90dd
Mona Lisa <lisa.nadeem@outlook.de>
2022-09-29 13:58:59 +0200

    imported MatIconModule and RouterModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { FormsModule } from "@angular/forms";
 import { UserRegistrationFormComponent } from "./user-registration-form/user-registration-form.component";
 import { UserLoginFormComponent } from "./user-login-form/user-login-form.component";
+import { RouterModule, Routes } from "@angular/router";
+import { MatIconModule } from "@angular/material/icon";
 
 // any features of Material Design that should be used in application will need to be included in app.module.ts file
 @NgModule({
