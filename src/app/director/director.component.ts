15280ec0d5ab0508394850ac9fb6509324bb81bb
Mona Lisa <lisa.nadeem@outlook.de>
2022-10-02 17:30:37 +0200

    changed Birthday to Birth in director TS file

--- src/app/director/director.component.ts
+++ src/app/director/director.component.ts
@@ -7,12 +7,17 @@ import { MAT_DIALOG_DATA } from "@angular/material/dialog";
   styleUrls: ["./director.component.scss"],
 })
 export class DirectorComponent implements OnInit {
+  /**
+   * @service injects data from the MovieCard component using the MAT_DIALOG_DATA injection token
+   * @example  {{ data.Birth }} would render the director's birth year
+   * @param data
+   */
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
       Name: string;
       Bio: string;
-      Birthday: Date;
+      Birth: Date;
     }
   ) {}
 
